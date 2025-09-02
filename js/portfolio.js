4a808092182290e5a4796a703c55670fa4478a3f
Song1524 <zsdx1525@gmail.com>
2025-09-02 21:05:26 +0900

    0902

--- js/portfolio.js
+++ js/portfolio.js
@@ -7,3 +7,19 @@ window.addEventListener('scroll', function () {
     const gaugeRatio = (html.scrollTop / total) * 100;
     gauge.style.width = gaugeRatio + '%';
 })
+
+new Swiper('.projects-swiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 18,
+    navigation: {
+    nextEl: '.projects-swiper .swiper-button-next',
+    prevEl: '.projects-swiper .swiper-button-prev',
+    },
+    speed: 800,
+    pagination: {
+        el: '.projects-swiper .swiper-pagination',
+        clickable: true
+    }
+});
